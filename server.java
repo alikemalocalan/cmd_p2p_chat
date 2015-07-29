e265e016f6f4a6278dbd017f6d6d0733e614aac9
Ali Kemal Öcalan <alikemalocalan@gmail.com>
2015-07-29 17:38:22 +0300

    kod temizligi

--- server.java
+++ server.java
@@ -1,87 +1,73 @@
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Key;
 import java.util.Scanner;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
-
 class sifreleme {
-    public static String secret = "TheBestSecretKey";
+    public static String secret = "theBestSecretKey";
     private static final String ALGO = "AES";
-    private static final byte[] keyValue = secret.getBytes();
 
-    public static String encrypt(String Data) throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGO);
+    String encrypt(String Data) throws Exception {
         Cipher c = Cipher.getInstance(ALGO);
-        c.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encVal = c.doFinal(Data.getBytes());
-        String encryptedValue = new BASE64Encoder().encode(encVal);
-        return encryptedValue;
+        c.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(secret.getBytes(), ALGO));
+        return new BASE64Encoder().encode(c.doFinal(Data.getBytes()));
     }
-    public static String decrypt(String encryptedData) throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGO);
+    String decrypt(String encryptedData) throws Exception {
         Cipher c = Cipher.getInstance(ALGO);
-        c.init(Cipher.DECRYPT_MODE, key);
-        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
-        byte[] decValue = c.doFinal(decordedValue);
-        String decryptedValue = new String(decValue);
-        return decryptedValue;
+        c.init(Cipher.DECRYPT_MODE, new SecretKeySpec(secret.getBytes(), ALGO));
+        return new String(c.doFinal(new BASE64Decoder().decodeBuffer(encryptedData)));
     }
 }
 class mesaj_oku extends Thread {
-    public void run() {
-    server s = new server();
-    sifreleme cryto = new sifreleme();
-    try {
-        while(true){//Mesaj karsiya ulasti
-            String okunanMesaj = cryto.decrypt(s.read.readUTF()); // gelen mesaji okuduk.
-            System.out.println(okunanMesaj);
+    public void run(){
+        while(true){
+            try {
+                System.out.println(new sifreleme().decrypt(new server().read.readUTF()));
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("kapatmak için bekleniyor");
+                new Scanner(System.in).next();
+            }
         }
-    } catch (Exception e) {
-        e.printStackTrace();
     }
-}}
+}
 class mesaj_gonder extends Thread {
-    public static Scanner input = new Scanner(System.in);
     public void run() {
-    server s = new server();
-    sifreleme cryto = new sifreleme();
-    try {
         while(true){//Mesaj karsiya ulasti
-            String mesaj = input.next();
-            s.write.writeUTF(cryto.encrypt(s.admin+" : " + mesaj + "\n"));
+            try {
+                new server().write.writeUTF(new sifreleme().encrypt("SERVER MESAJ" + " : " + new Scanner(System.in).next() + "\n"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    } catch (Exception e) {
-        e.printStackTrace();
     }
-}}
+}
 class server {
-    public static Scanner input = new Scanner(System.in);
-    public static String kullanici,admin;
-    public static DataOutputStream write;
     public static DataInputStream read;
+    public static DataOutputStream write;
+    public static Socket client;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         try {
-            System.out.println("Kullanici adiniz: ");
-            admin= input.next();
-            ServerSocket server = new ServerSocket(42222); // portu dinlenilmeye
-            System.out.println("Client Bekleniyor");
-            Socket client=server.accept(); // Su an bize client  bekliyoruz.
-            System.out.println("Client baglandi "+client.getLocalAddress()); // Client baglandi
-
+            client=new ServerSocket(42222).accept();
+            System.out.println(client.getLocalAddress());
             read = new DataInputStream(client.getInputStream());
-            write = new DataOutputStream(client.getOutputStream()); //Clienta mesaj yollamak icin
-        } catch (IOException e) {
+            write= new DataOutputStream(client.getOutputStream());
+        }
+
+         catch (Exception e) {
             e.printStackTrace();
         }
+
         new mesaj_oku().start();
         new mesaj_gonder().start();
-    }
+
+}
 }
