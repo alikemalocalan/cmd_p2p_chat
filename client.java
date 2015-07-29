65ee8041b1a0cac291e2a1095b410b6acc8c95d9
Ali Kemal Öcalan <alikemalocalan@gmail.com>
2015-07-29 17:37:24 +0300

    kod temizligi

--- client.java
+++ client.java
@@ -1,84 +1,59 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
-
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
 
 class sifreleme {
-    public static String secret = "TheBestSecretKey";
+    public static String secret = "theBestSecretKey";
     private static final String ALGO = "AES";
-    private static final byte[] keyValue = secret.getBytes();
 
-    public static String encrypt(String Data) throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGO);
+    String encrypt(String Data) throws Exception {
         Cipher c = Cipher.getInstance(ALGO);
-        c.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encVal = c.doFinal(Data.getBytes());
-        String encryptedValue = new BASE64Encoder().encode(encVal);
-        return encryptedValue;
+        c.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(secret.getBytes(), ALGO));
+        return new BASE64Encoder().encode(c.doFinal(Data.getBytes()));
     }
-    public static String decrypt(String encryptedData) throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGO);
+    String decrypt(String encryptedData) throws Exception {
         Cipher c = Cipher.getInstance(ALGO);
-        c.init(Cipher.DECRYPT_MODE, key);
-        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
-        byte[] decValue = c.doFinal(decordedValue);
-        String decryptedValue = new String(decValue);
-        return decryptedValue;
+        c.init(Cipher.DECRYPT_MODE, new SecretKeySpec(secret.getBytes(), ALGO));
+        return new String(c.doFinal(new BASE64Decoder().decodeBuffer(encryptedData)));
     }
 }
 class mesaj_oku extends Thread {
-    public void run() {
-        client s = new client();
-        sifreleme cryto = new sifreleme();
-        try {
-            while(true){//Mesaj karsiya ulasti
-                String okunanMesaj = cryto.decrypt(s.read.readUTF()); // gelen mesaji okuduk.
-                System.out.println(okunanMesaj);
+    public void run(){
+        while(true){//Mesaj karsiya ulasti
+            try {
+                System.out.println(new sifreleme().decrypt(new client().read.readUTF()));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
 class mesaj_gonder extends Thread {
-    public static Scanner input = new Scanner(System.in);
     public void run() {
-        client s = new client();
-        sifreleme cryto = new sifreleme();
-        try {
-            while(true){//Mesaj karsiya ulasti
-                String mesaj = input.next();
-                s.write.writeUTF(cryto.encrypt(s.kullanici+" : " +mesaj + "\n"));
+        while(true){//Mesaj karsiya ulasti
+            try {
+                new client().write.writeUTF(new sifreleme().encrypt("Kullanici" + " : " + new Scanner(System.in).next() + "\n"));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
 class client{
-    public static Scanner input = new Scanner(System.in);
-    public static String kullanici;
     public static DataOutputStream write;
     public static DataInputStream read;
 
     public static void main(String args[]) throws IOException {
-        try {
-            System.out.println("Kullanici adiniz: ");
-            kullanici= input.next();
-
-            String ip="127.0.0.1"; // Hangi servera baglanmak istedigimizi yaziyoruz. Ben local deki baglanacagim
-            Socket soket = new Socket(ip, 42222); //Servera baglandim.
-            read = new DataInputStream(soket.getInputStream());
-            write = new DataOutputStream(soket.getOutputStream()); //Clienta mesaj yollamak icin
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("port baglanmadı");
-        }
+        Socket soket = new Socket("127.0.0.1", 42222); //Servera baglandim.
+        read = new DataInputStream(soket.getInputStream());
+        write = new DataOutputStream(soket.getOutputStream()); //Clienta mesaj yollamak icin
+        //Eger ki mesaj alsaydik DataInputStream  kullanacaktik.
         new mesaj_oku().start();
         new mesaj_gonder().start();
     }
